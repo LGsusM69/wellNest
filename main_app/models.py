a0ce188ef7073a7e336f7c6143193457ce469a9f
JesusM97 <jissousklp@gmail.com>
2024-03-18 11:28:01 -0600

    created db models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    userName = models.CharField(max_length=100)
+    passWord = models.CharField()
+    email = models.CharField()
+
+    
+class Journal(models.Model):
+    dailyPrompt = models.TextField(max_length=250)
+    freeWritte = models.TextField(max_length=1000)
+
+class DailyCheckIn(models.Model):
+    
