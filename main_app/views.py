419bf7e1084a01699f1294f8ead3727746e26c70
MrVictor2 <v.suarez6@gmail.com>
2024-03-19 16:00:22 -0400

    add style to base file

--- main_app/views.py
+++ main_app/views.py
@@ -8,18 +8,10 @@ from .models import Journal, DailyCheckIn, Plans
 
 
 def home(request):
-    if request.method == 'POST':
-        mood_form = MoodCheckForm(request.POST)
     return render(request, 'home.html')
 
-def dashboard(request):
-    return render(request, 'features/dashboard.html')
-
 def checkins(request):
-
-    return render(request, 'features/checkins.html', {
-        'mood_form': MoodCheckForm()
-    })
+    return render(request, 'features/checkins.html')
 
 def journal(request):
     current_date = date.today()
