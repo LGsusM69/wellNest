e23620bc4f23f7e84e6b90a4598004370196797d
JesusM97 <jissousklp@gmail.com>
2024-03-19 15:11:57 -0600

    add create plan functionallity

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import DailyCheckIn, Journal, User, DailySummary
+from .models import DailyCheckIn, Journal, User, DailySummary, Plan
 
 # Register your models here.
 
 admin.site.register(DailyCheckIn)
 admin.site.register(Journal)
 admin.site.register(DailySummary)
+admin.site.register(Plan)
