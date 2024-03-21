cbc785451e91de675dba7e14e9961cc21ebddfd0
mursal-r <mursalrahimi.ga@gmail.com>
2024-03-21 15:29:10 -0700

    confirm merge

--- wellNest/settings.py
+++ wellNest/settings.py
@@ -146,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
